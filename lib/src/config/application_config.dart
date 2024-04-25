728283a43684e96dc6e1de8c46365ffb880b2792
Barba2k2 <lorenzo.dz@hotmail.com>
2024-04-25 17:21:33 -0300

    Added database config and logger load on application config

--- lib/src/config/application_config.dart
+++ lib/src/config/application_config.dart
@@ -1,13 +1,31 @@
-import 'dart:developer';
-
 import 'package:dotenv/dotenv.dart' show load, env;
+import 'package:get_it/get_it.dart';
+
+import '../logger/i_logger.dart';
+import '../logger/logger.dart';
+
+import 'databse_conncetion_configuration.dart';
 
 class ApplicationConfig {
   Future<void> loadConfigApplication() async {
     await _loadEnv();
-    final variavel = env['url_banco_de_dados'];
-    log('$variavel');
+    _loadDatabaseConfig();
+    _configLogger();
   }
 
   Future<void> _loadEnv() async => load();
+
+  void _loadDatabaseConfig() {
+    final databseConfig = DatabseConncetionConfiguration(
+      host: env['DATABASE_HOST'] ?? env['databaseHost']!,
+      user: env['DATABASE_USER'] ?? env['databaseUser']!,
+      port: int.tryParse(env['DATABASE_PORT'] ?? env['databasePort']!) ?? 0,
+      password: env['DATABASE_PASSWORD'] ?? env['databasePassword']!,
+      databaseName: env['DATABASE_NAME'] ?? env['databaseName']!,
+    );
+    GetIt.I.registerSingleton(databseConfig);
+  }
+
+  void _configLogger() =>
+      GetIt.I.registerLazySingleton<ILogger>(() => Logger());
 }
