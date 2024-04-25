e3743c0ac669df0fed39ebd27333f8cbb554a943
Barba2k2 <lorenzo.dz@hotmail.com>
2024-04-25 15:49:19 -0300

    created inital config of the API

--- lib/src/config/application_config.dart
+++ lib/src/config/application_config.dart
@@ -1,8 +1,12 @@
-import 'package:dotenv/dotenv.dart' show load;
+import 'dart:developer';
+
+import 'package:dotenv/dotenv.dart' show load, env;
 
 class ApplicationConfig {
   Future<void> loadConfigApplication() async {
     await _loadEnv();
+    final variavel = env['url_banco_de_dados'];
+    log('$variavel');
   }
 
   Future<void> _loadEnv() async => load();
