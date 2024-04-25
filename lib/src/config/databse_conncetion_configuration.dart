f634de245420cc50f44714c0286aba46fc6fe6a8
Barba2k2 <lorenzo.dz@hotmail.com>
2024-04-25 17:21:54 -0300

    Created the configuration of the db

---
+++ lib/src/config/databse_conncetion_configuration.dart
@@ -0,0 +1,15 @@
+class DatabseConncetionConfiguration {
+  final String host;
+  final String user;
+  final int port;
+  final String password;
+  final String databaseName;
+
+  DatabseConncetionConfiguration({
+    required this.host,
+    required this.user,
+    required this.port,
+    required this.password,
+    required this.databaseName,
+  });
+}
