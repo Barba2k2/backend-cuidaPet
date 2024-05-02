ddace4763d42b01f54ad4a1891ddebc0cd4cba03
Barba2k2 <lorenzo.dz@hotmail.com>
2024-05-02 10:17:10 -0300

    created initial configuration of supplier router

--- lib/app/config/service_locator_config.config.dart
+++ lib/app/config/service_locator_config.config.dart
@@ -9,19 +9,24 @@ import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
 
 import '../../modules/categories/controller/categories_controller.dart' as _i3;
-import '../../modules/categories/data/categories_repository.dart' as _i17;
-import '../../modules/categories/data/i_categories_repository.dart' as _i16;
+import '../../modules/categories/data/categories_repository.dart' as _i22;
+import '../../modules/categories/data/i_categories_repository.dart' as _i21;
 import '../../modules/categories/service/categories_service.dart' as _i5;
 import '../../modules/categories/service/i_categories_service.dart' as _i4;
-import '../../modules/user/controller/auth_controller.dart' as _i15;
-import '../../modules/user/controller/user_controller.dart' as _i14;
-import '../../modules/user/data/i_user_repository.dart' as _i9;
-import '../../modules/user/data/user_repository.dart' as _i10;
-import '../../modules/user/service/I_user_service.dart' as _i12;
-import '../../modules/user/service/user_service.dart' as _i13;
+import '../../modules/supplier/controller/supplier_controller.dart' as _i18;
+import '../../modules/supplier/data/i_supplier_repository.dart' as _i9;
+import '../../modules/supplier/data/supplier_repository.dart' as _i10;
+import '../../modules/supplier/service/i_supplier_service.dart' as _i11;
+import '../../modules/supplier/service/supplier_service.dart' as _i12;
+import '../../modules/user/controller/auth_controller.dart' as _i20;
+import '../../modules/user/controller/user_controller.dart' as _i19;
+import '../../modules/user/data/i_user_repository.dart' as _i13;
+import '../../modules/user/data/user_repository.dart' as _i14;
+import '../../modules/user/service/I_user_service.dart' as _i16;
+import '../../modules/user/service/user_service.dart' as _i17;
 import '../database/database_connection.dart' as _i7;
 import '../database/i_database_connection.dart' as _i6;
-import '../logger/i_logger.dart' as _i11;
+import '../logger/i_logger.dart' as _i15;
 import 'database_connection_configuration.dart'
     as _i8; // ignore_for_file: unnecessary_lambdas
 
@@ -43,25 +48,28 @@ _i1.GetIt $initGetIt(
       () => _i5.CategoriesService(repository: get<InvalidType>()));
   gh.lazySingleton<_i6.IDatabaseConnection>(
       () => _i7.DatabaseConnection(get<_i8.DatabaseConnectionConfiguration>()));
-  gh.lazySingleton<_i9.IUserRepository>(() => _i10.UserRepository(
+  gh.lazySingleton<_i9.ISupplierRepository>(() => _i10.SupplierRepository());
+  gh.lazySingleton<_i11.ISupplierService>(() => _i12.SupplierService());
+  gh.lazySingleton<_i13.IUserRepository>(() => _i14.UserRepository(
         connection: get<_i6.IDatabaseConnection>(),
-        log: get<_i11.ILogger>(),
+        log: get<_i15.ILogger>(),
       ));
-  gh.lazySingleton<_i12.IUserService>(() => _i13.UserService(
-        userRepository: get<_i9.IUserRepository>(),
-        log: get<_i11.ILogger>(),
+  gh.lazySingleton<_i16.IUserService>(() => _i17.UserService(
+        userRepository: get<_i13.IUserRepository>(),
+        log: get<_i15.ILogger>(),
       ));
-  gh.factory<_i14.UserController>(() => _i14.UserController(
-        userService: get<_i12.IUserService>(),
-        log: get<_i11.ILogger>(),
+  gh.factory<_i18.SupplierController>(() => _i18.SupplierController());
+  gh.factory<_i19.UserController>(() => _i19.UserController(
+        userService: get<_i16.IUserService>(),
+        log: get<_i15.ILogger>(),
       ));
-  gh.factory<_i15.AuthController>(() => _i15.AuthController(
-        userService: get<_i12.IUserService>(),
-        log: get<_i11.ILogger>(),
+  gh.factory<_i20.AuthController>(() => _i20.AuthController(
+        userService: get<_i16.IUserService>(),
+        log: get<_i15.ILogger>(),
       ));
-  gh.lazySingleton<_i16.ICategoriesRepository>(() => _i17.CategoriesRepository(
+  gh.lazySingleton<_i21.ICategoriesRepository>(() => _i22.CategoriesRepository(
         connection: get<_i6.IDatabaseConnection>(),
-        log: get<_i11.ILogger>(),
+        log: get<_i15.ILogger>(),
       ));
   return get;
 }
---
+++ lib/modules/supplier/supplier_router.dart
@@ -0,0 +1,14 @@
+import 'package:get_it/get_it.dart';
+import 'package:shelf_router/src/router.dart';
+
+import 'controller/supplier_controller.dart';
+import '../../app/routers/i_router.dart';
+
+class SupplierRouter implements IRouter {
+  @override
+  void configure(Router router) {
+    final supplierController = GetIt.I.get<SupplierController>();
+
+    router.mount('/suppliers/', supplierController.router);
+  }
+}
